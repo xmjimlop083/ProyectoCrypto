a029a361a9cd03a24560e167b42560cf6fcefdc6
xmjimlop083 <xmjimlop083@ieshnosmachado.org>
2026-02-05 09:29:30 +0100

    5 de febrero

--- crypto-pro/src/app/components/home/home.ts
+++ crypto-pro/src/app/components/home/home.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Crypto, CryptoCurrency } from '../../services/crypto';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+  featuredCryptos: CryptoCurrency[] = [];
 
+  constructor(private cryptoService: Crypto) {}
+
+  ngOnInit(): void {
+    this.cryptoService.getCryptos().subscribe({
+      next: (data) => {
+        this.featuredCryptos = data.slice(0, 3);
+      },
+      error: (err) => {
+        console.error('Error fetching cryptos:', err);
+      },
+    });
+  }
 }
